c10cc764e8b224d161cfbc50611de14dd289c170
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-10-05 09:16:36 -0400

    added script that generates more rows in the table for css testing

--- js/js.js
+++ js/js.js
@@ -6,3 +6,9 @@
 function lol(a, b) {
     a.style.color = b;
 }
+
+const body = document.querySelector('tbody');
+
+for (let i = 0; i < 50; i++) {
+    body.innerHTML += "<tr><td>proj1</td><td>nasr</td><td>js320 prog</td><td>pract</td><td>completed</td><td>20</td><td>1.5</td><td>2 weeks</td></tr>"
+}
