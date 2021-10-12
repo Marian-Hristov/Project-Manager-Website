eae54612027f1a73cfd36cfcb9bb816e1bd25c03
DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>
2021-10-11 21:02:53 -0400

    trying to fix PointerEvent error

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,14 @@
 function enablePopUpClose() {
     let types = ["action", "add"]
     types.forEach(type => {
-        document.querySelector(`.pop-up.${type} .pop-up-close svg`).addEventListener("click", () => {
+        document.querySelector(`svg.${type}`).addEventListener("click", function () {
             closePopUp(type);
         });
     })
 }
 
 function closePopUp(type) {
+    console.log(type);
     document.querySelector(`.pop-up.${type}`).style.display = "none";
     document.querySelector(".main-container").style.overflow = "auto";
 }
