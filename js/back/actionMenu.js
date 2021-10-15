6f71675692553d2c2a7faf84e1a33cf8108738ec
DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>
2021-10-15 11:38:33 -0400

    added cmts for actionMenu and storage

--- js/back/actionMenu.js
+++ js/back/actionMenu.js
@@ -1,19 +1,41 @@
+/**
+ * This function starts editing the current project
+ * @param {number} index the index of the edited project
+ */
 function startEditingProject(index) {
+    // Set the current project to the index of the clicked project
     currentEditedProject = index;
+    // Opening edit popup
     openPopUp("action");
+    // Getting the project and putting in the values on the form
     let project = allProjects[currentEditedProject];
     getProjectToForm(project);
 }
 
+/**
+ * This function stops the editing of the current project
+ * and sends the information to the storage and updates the table
+ * @param {number} currentPage The current page of the table
+ */
 function stopEditingProject(currentPage) {
+    // Getting the values in form a project object
     let newProject = getFormToProject("action");
+    // Adding the new project at the place of the old project
     allProjects[currentEditedProject] = newProject;
+    // Overwriting local storage
     writeLocal();
+    // Resetting the current edited project index
     currentEditedProject = undefined;
+    // Closing the edit popup
     closePopUp("action");
+    // Showing the new updated table
     showTable(currentPage);
 }
-
+/**
+ * This function deletes a project and updates the table accordingly
+ * @param {*} index The index of the project that has be deleted
+ * @param {*} currentPage The current page of the table
+ */
 function deleteProject(index, currentPage) {
     // const projects = getProjects();
     const maxPageCount = Number(document.querySelector(".table-page-change-input span").textContent.slice(3));
@@ -28,101 +50,7 @@ function deleteProject(index, currentPage) {
     if (currentPage == 0) {
         currentPage = 1;
     }
+    // Updating the storage and the table
     writeLocal(allProjects);
     showTable(currentPage);
-}
-
-function createActionMenu(index) {
-    const xmlns = "http://www.w3.org/2000/svg";
-
-    // Td holding the whole action menu
-    let box = document.createElement("td");
-    box.classList.add("action-box")
-
-    // The action-menu button
-    let button = document.createElement("button");
-    button.classList.add("action-button");
-    // Button SVG
-    // let svg = document.createElement("svg");
-    let svgDots = document.createElement("svg");
-    svgDots.classList.add("icon");
-    svgDots.setAttribute("viewBox", "0 0 16 16");
-    // SVG CIRCLES
-    let arr = ["8", "1.5", "14.5"];
-    arr.forEach(elem => {
-        let circle = document.createElement("circle");
-        circle.setAttribute("cx", elem);
-        circle.setAttribute("cy", "7.5");
-        circle.setAttribute("r", "1.5");
-        svgDots.appendChild(circle);
-    })
-    button.appendChild(svgDots);
-    box.appendChild(button);
-
-    // Action-menu
-    // div
-    let menu = document.createElement("div");
-    menu.classList.add("action-menu");
-    // ul
-    let ul = document.createElement("ul");
-    // li Edit
-    let li = document.createElement("li");
-    li.classList.add("option");
-    li.setAttribute("index", `${index}`);
-    let svgEdit = document.createElement("svg");
-    svgEdit.setAttribute("aria-hidden", "true");
-    svgEdit.setAttribute("focusable", "false");
-    svgEdit.setAttribute("data-prefix", "far");
-    svgEdit.setAttribute("data-icon", "edit");
-    svgEdit.setAttribute("class", "svg-inline--fa fa-edit fa-w-18");
-    svgEdit.setAttribute("role", "img");
-    svgEdit.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svgEdit.setAttribute("viewBox", "0 0 576 512");
-    let pathEdit = document.createElement("path");
-    pathEdit.setAttribute("fill", "currentColor");
-    pathEdit.setAttribute("d", "M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z")
-
-    svgEdit.appendChild(pathEdit);
-    let p = document.createElement("p")
-    p.textContent = "Edit";
-
-    li.appendChild(svgEdit);
-    li.appendChild(p);
-
-    ul.appendChild(li)
-
-    //li Delete
-    li = document.createElement("li");
-    li.classList.add("option");
-    li.addEventListener("click", function () {
-        console.log("works");
-        deleteProject(index);
-    })
-    let svgDelete = document.createElement("svg");
-    svgDelete.setAttribute("aria-hidden", "true");
-    svgDelete.setAttribute("focusable", "false");
-    svgDelete.setAttribute("data-prefix", "fas");
-    svgDelete.setAttribute("data-icon", "trash-alt");
-    svgDelete.setAttribute("class", "svg-inline--fa fa-trash-alt fa-w-14");
-    svgDelete.setAttribute("role", "img");
-    svgDelete.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svgDelete.setAttribute("viewBox", "0 0 448 512");
-    let pathDelete = document.createElement("path");
-    pathDelete.setAttribute("fill", "currentColor");
-    pathDelete.setAttribute("d", "M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z")
-
-    svgDelete.appendChild(pathDelete);
-    p = document.createElement("p")
-    p.textContent = "Delete";
-
-    li.appendChild(svgDelete);
-    li.appendChild(p);
-
-    ul.appendChild(li);
-
-    menu.appendChild(ul);
-    box.appendChild(menu);
-
-    console.log(box);
-    return box;
 }
--- js/back/storage.js
+++ js/back/storage.js
@@ -1,10 +1,13 @@
+/**
+ * This function adds the current projects array to the local storage
+ */
 function writeLocal() {
     updateStatusBar(`Saving ${allProjects.length} to Local Storage`);
     localStorage.setItem(0, JSON.stringify(allProjects));
 }
 
 /**
- * clears the storage
+ * Clears the storage
  */
 function clearStorage() {
     updateStatusBar(`Local Storage has been cleared`);
