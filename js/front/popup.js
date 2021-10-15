a3aeefeac293316aa340c79fdc771974e9452de2
DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>
2021-10-15 12:04:14 -0400

    added comments for popup

--- js/front/popup.js
+++ js/front/popup.js
@@ -1,5 +1,7 @@
 'use strit';
-
+/**
+ * This function adds EventListeners for the close button
+ */
 function enablePopUpClose() {
     let types = ["action", "add"]
     types.forEach(type => {
@@ -8,12 +10,18 @@ function enablePopUpClose() {
         });
     })
 }
-
+/**
+ * This function closes the given popup
+ * @param {string} type the type of the popup
+ */
 function closePopUp(type) {
     document.querySelector(`.pop-up.${type}`).style.display = "none";
     document.body.style.overflow = "auto";
 }
-
+/**
+ * This function opens the given popup
+ * @param {string} type the type of the popup
+ */
 function openPopUp(type) {
     document.querySelector(`.pop-up.${type}`).style.display = "flex";
     document.body.style.overflow = "hidden";
